9d8c9587b9e6ead03dc7ddbbc8b2ce7ffc2a6ee9
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-11 17:33:59 -0400

    add show route for rooms connectsTo and current dinosaurs in room

--- controllers/roomsController.js
+++ controllers/roomsController.js
@@ -15,12 +15,36 @@ roomsRouter.get("/", (req, res) => {
     }
 })
 
-// Show Route
+// Show Route For Each Room
 roomsRouter.get("/:id", (req, res) => {
     const { id } = req.params
     const room = roomArray.find((roomArray) => roomArray.roomId === id)
     if (room) {
             res.status(200).send(room)
+    } else {
+        // res.status(404).json({error: `Room with id ${id} does not exist.`})
+        res.send("Cannot find any Room with this id: " + id)
+    }
+})
+
+// Show Route For Each Rooms Connect To
+roomsRouter.get("/:id/connectsTo", (req, res) => {
+    const { id } = req.params
+    const room = roomArray.find((roomArray) => roomArray.roomId === id)
+    if (room) {
+            res.status(200).send(room.connectsTo)
+    } else {
+        // res.status(404).json({error: `Room with id ${id} does not exist.`})
+        res.send("Cannot find any Room with this id: " + id)
+    }
+})
+
+// Show Route For Each Rooms Displayed Dinosaur
+roomsRouter.get("/:id/dinosaurs", (req, res) => {
+    const { id } = req.params
+    const room = roomArray.find((roomArray) => roomArray.roomId === id)
+    if (room) {
+            res.status(200).send(room.dinosaurs)
     } else {
         // res.status(404).json({error: `Room with id ${id} does not exist.`})
         res.send("Cannot find any Room with this id: " + id)
