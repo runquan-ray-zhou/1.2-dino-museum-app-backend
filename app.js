58fb36a80d29a28abd6238a9d0e852ebd8983da1
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-11 17:16:59 -0400

    set up app.js and server.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// Dependencies
+const express = require("express")
+const cors = require("cors")
+
+// Configuration
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Controllers
+// const roomsController = require("./controllers/roomsController")
+// const dinosaursController = require("./controllers/dinosaursController")
+
+// app.use("/rooms", roomsController)
+// app.use("/dinosaurs", dinosaursController)
+
+// Root/Health Check Route
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to the Dinosaur Museum App Backend!")
+})
+
+// 404 Page Not Found Route
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Sorry, no page found" });
+});
+
+// Export App
+module.exports = app
